9738c6dcf34177c41218f71c2a36a57efee6938e
Nigel Grech <nigelgrech.ng@gmail.com>
2022-05-24 13:23:44 +0200

    Add function to split song into n samples

--- src/music_preprocess.py
+++ src/music_preprocess.py
@@ -1,11 +1,18 @@
-
+import librosa
 
 
 from unicodedata import name
 
 
-def sample_song(song_pth:str) -> list:
-    pass
+def sample_song(song_pth:str, sample_rate:int=22050, n_samples:int=3) -> list:
+    """Loads a song from a given path, and divides the song into
+    n_samples of equal length.
+    """
+    song, _ = librosa.load(song_pth, sr=sample_rate)
+    n_frames = len(song)
+    fames_per_sample = n_frames//3
+
+    return [song[fames_per_sample*i:fames_per_sample*(i+1)] for i in range(3)]
 
 def convert_to_QC_spectrogram(sample):
     # takes wav sample and returns spectrogram img.
