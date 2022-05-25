793f4234452ab99a4975e4c4b3f95f46781b85e3
Nigel Grech <nigelgrech.ng@gmail.com>
2022-05-25 08:25:10 +0200

    Add batch process Add chcq dependance

--- src/music_preprocess.py
+++ src/music_preprocess.py
@@ -1,37 +1,43 @@
 # General imports
-from traceback import print_tb
-from unicodedata import name
+import os
+from logging import root
+
+import librosa
+import matplotlib
 import numpy as np
 
-# Music preprocessing imports
+matplotlib.use('Agg')
+
 import cqhc
 import librosa
+import matplotlib.pyplot as plt
+
 
 def sample_song(song_pth:str, sample_rate:int=22050, n_samples:int=3) -> list:
     """Loads a song from a given path, and divides the song into
     n_samples of equal length.
     """
     song, _ = librosa.load(song_pth, sr=sample_rate)
+    if n_samples == 1:
+        return [song]
     n_frames = len(song)
-    fames_per_sample = n_frames//3
+    fames_per_sample = n_frames//int(n_samples)
 
-    return [song[fames_per_sample*i:fames_per_sample*(i+1)] for i in range(3)]
+    return [song[fames_per_sample*i:fames_per_sample*(i+1)] for i in range(n_samples)]
 
-def convert_to_QC_spectrogram(sample_name, sub_sample_time=True):
+def convert_to_QC_spectrogram(audio_signal, sampling_frequency, sub_sample_time=True):
     
     """
     Converts the music category wav-files to constant-Q harmonic coefficients (CQTHCs), using
     the CQHC-Python package: https://github.com/zafarrafii/CQHC-Python
     
     Args:
-        sample_name      (str):   Full path to the WAV-file 
-        sub_sample_time  (bool):  Switch for subsampling in time
+        audio_signal      (np.array):   Loaded wave file as an np array
+        sampling frequency (int):       HZ at with the audio file was sampled   
+        sub_sample_time   (bool):       Switch for subsampling in time
     """
 
     # takes wav sample and returns spectrogram img.
-
-    # Load the audio file
-    audio_signal, sampling_frequency = librosa.load(sample_name, sr=None, mono=True)
 
     # Define the parameters and compute the CQT spectrogram
     step_length = int(pow(2, int(np.ceil(np.log2(0.04 * sampling_frequency)))) / 2)
@@ -64,15 +70,67 @@ def convert_to_QC_spectrogram(sample_name, sub_sample_time=True):
         # Truncate in freq dims to 256
         cqts_2 = cqt_spectrogram.copy()
         h0 = cqts_2.shape[0]
-        dh = np.int(np.ceil((h0 - h1) / 2))
+        dh = int(np.ceil((h0 - h1) / 2))
         cqts_3 = cqts_2.copy()
         cqts_3 = cqts_3[dh:h0-dh, :]
 
     return cqts_3
 
-def batch_process():
-    # process all songs.
-    pass
+def convert_to_chromagram(sample, sample_rate):
+    return librosa.feature.chroma_stft(y=sample, sr=sample_rate)
 
-if __name__=='main':
-    batch_process()
+def batch_process_songs_to_spectrograms(root_dir:str, output_dir:str, genres:list=None, sample_rate:int=22050, splits=3, mode:str='chromagram'):
+    """Process all songs in root_dir to 3 even length sample spectrograms"""
+
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
+
+    if not genres:
+        genres = os.listdir(root_dir)
+
+    for genre in genres:
+        pth = os.path.join(root_dir, genre)
+        song_paths = [os.path.join(pth, s) for s in os.listdir(pth)]
+        process_songs(
+            song_paths,
+            output_dir,
+            genre,
+            sample_rate,
+            splits,
+            mode
+        )
+
+def process_songs(song_paths:list, out_path:str, genre:str, sample_rate:int=22050, splits=3, mode:str='chromagram'):
+    chroma_dir = os.path.join(out_path, 'chromagram', genre, f'{sample_rate}Hz', f'{splits}-splits')
+    constant_q_dir = os.path.join(out_path, 'constant-q', genre, f'{sample_rate}Hz', f'{splits}-splits')
+    for i, song_path in enumerate(song_paths):
+        for j, sample in enumerate(sample_song(song_path, n_samples=splits)):
+            if mode == 'chromagram' or mode == 'both':
+                if not os.path.isdir(chroma_dir):
+                    os.makedirs(chroma_dir)
+                spect = convert_to_chromagram(sample, sample_rate)
+                pth =  os.path.join(chroma_dir, f'{i:003d}-{j:002d}.png')
+                save_spectrogram(spect, pth)
+            if mode == 'constant-q' or mode == 'both':
+                if not os.path.isdir(constant_q_dir):
+                    os.makedirs(constant_q_dir)
+                pth =  os.path.join(constant_q_dir, f'{i:003d}-{j:002d}.png')
+                spect = convert_to_QC_spectrogram(sample, sample_rate, sub_sample_time=False)
+                save_spectrogram(spect, pth)
+                pass
+
+def save_spectrogram(spect, pth:str):
+    fig = plt.figure()
+    plt.figure(figsize=(5, 5))
+    plt.pcolormesh(spect, cmap='binary')
+    plt.axis('off')
+    plt.savefig(pth, bbox_inches='tight', pad_inches=0, dpi=300)
+    plt.close(fig)
+    plt.close('all')
+
+if __name__=='__main__':
+    root = os.path.join('data','GTZAN','genres_original')
+    output = os.path.join('data', 'Spectrograms')
+    genres = ['blues', 'jazz', 'pop', 'metal']
+    batch_process_songs_to_spectrograms(root, output, genres, mode='constant-q')
+
