d15def7af5ee90ee58faaed5e1649dc9802ca796
Nigel Grech <nigelgrech.ng@gmail.com>
2022-05-25 09:45:38 +0200

    add init def dummy for data loader

--- src/dataset.py
+++ src/dataset.py
@@ -2,10 +2,13 @@ from torch.utils.data import Dataset
 
 def generate_pairings_csv(output_path:str):
     # generate a csv file for pairing to output path
+    # <emote img path>,<spectrogram pth>,<set>
     pass
 
-class EmotePairingDataset():
-    pass
+class EmotePairingDataset(Dataset):
+
+    def __init__(self, parings_pth, set:str='train', transform=None, target_transform=None):
+        pass
 
 if __name__=='main':
     generate_pairings_csv()
